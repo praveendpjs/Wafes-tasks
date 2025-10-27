38494dac412c34919e3c651b2328b06c9811b10c
praveendpjs <praveenaishu2012@gmail.com>
2025-10-27 21:58:40 +0530

    register and login component added

--- personal-dashboard-app/src/app/app.routes.ts
+++ personal-dashboard-app/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './Components/home/home';
 import { About } from './Components/about/about';
 import { Contact } from './Components/contact/contact';
+import { Login } from './Components/login/login';
+import { Register } from './Components/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'about', component: About },
-  { path: 'contact', component: Contact }
+  { path: 'contact', component: Contact },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+
 ];
