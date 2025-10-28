60f17621efd33360ea946ed8b40f639f9604fbfe
praveendpjs <praveenaishu2012@gmail.com>
2025-10-28 06:39:24 +0530

    hash eye features added

--- personal-dashboard-app/src/app/Components/register/register.ts
+++ personal-dashboard-app/src/app/Components/register/register.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ ReactiveFormsModule,CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  registerForm: FormGroup;
+  showPassword = false;
+  showConfirmPassword = false;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+    togglePasswordVisibility(field: string) {
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    } else if (field === 'confirm') {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      console.warn('Form is invalid');
+    }
+  }
 }
