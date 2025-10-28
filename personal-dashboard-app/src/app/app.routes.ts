bacf0f1f3d7d435cc988f13df03b3864a5a03524
praveendpjs <praveenaishu2012@gmail.com>
2025-10-28 14:13:57 +0530

    authguard added

--- personal-dashboard-app/src/app/app.routes.ts
+++ personal-dashboard-app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Contact } from './Components/contact/contact';
 import { Login } from './Components/login/login';
 import { Register } from './Components/register/register';
 import { Dashboard } from './Components/dashboard/dashboard';
+import { authGuard } from './Components/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -13,7 +14,7 @@ export const routes: Routes = [
   { path: 'contact', component: Contact },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'dashboard', component: Dashboard },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard]},
 
   { path: '**', redirectTo: 'register' },
 
